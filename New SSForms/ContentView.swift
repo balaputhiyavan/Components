347519fab7d78d35cf4ece925758a6c40a06d089
balaputhiyavan <balaputhiyavan4@gmail.com>
2023-01-09 11:18:12 +0530

    Initial Commit

---
+++ New SSForms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  New SSForms
+//
+//  Created by Zetta_Pc on 09/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ New SSForms/New_SSFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_SSFormsApp.swift
+//  New SSForms
+//
+//  Created by Zetta_Pc on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct New_SSFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
